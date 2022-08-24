100391bcd18f10d8423317cd9687e9f93870fa7f
pedro-bz <benzinapedro@gmail.com>
2022-08-24 11:27:42 -0300

    Finished exercise 1 and 2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,6 +2,10 @@
 
 
 def maximo_basico(a: float, b: float) -> float:
+    if a > b:
+        return a
+    return b
+
     """Toma dos números y devuelve el mayor.
 
     Restricciones:
@@ -9,7 +13,6 @@ def maximo_basico(a: float, b: float) -> float:
         - No utilizar ELSE
         - No utilizar la función max
     """
-
 
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
@@ -21,10 +24,11 @@ assert maximo_basico(9, 18) == 18
 
 
 def maximo_libreria(a: float, b: float) -> float:
+    return max(a, b)
+
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
-
 
 # NO MODIFICAR - INICIO
 assert maximo_libreria(10, 5) == 10
@@ -36,10 +40,11 @@ assert maximo_libreria(9, 18) == 18
 
 
 def maximo_ternario(a: float, b: float) -> float:
+    return a if a>b else b
+
     """Re-escribir utilizando el operador ternario.
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
     """
-
 
 # NO MODIFICAR - INICIO
 assert maximo_ternario(10, 5) == 10
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -3,6 +3,12 @@
 
 
 def maximo_encadenado(a: float, b: float, c: float) -> float:
+    if (a>b):
+        return max(a, c)
+    if (b>c):
+        return b
+    return c
+
     """Toma 3 números y devuelve el máximo.
 
     Restricciones:
@@ -34,9 +40,10 @@ assert maximo_encadenado(24, 18, 18) == 24
 
 
 def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
-    """Re-escribir para que tome 4 parámetros, utilizar la función max.
+    return max(a, b, c, d)
 
-    Referencia: https://docs.python.org/3/library/functions.html#max"""
+    """Re-escribir para que tome 4 parámetros, utilizar la función max.
+    Referencia: https://docs.python.org/3/library/functions.html#max """
 
 
 # NO MODIFICAR - INICIO
@@ -51,10 +58,10 @@ assert maximo_cuadruple(24, 9, 18, 30) == 30
 
 
 def maximo_arbitrario(*args) -> float:
-    """Re-escribir para que tome una cantidad arbitraria de parámetros.
-    Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists # noqa: E501
-    """
+    return max(*args)
 
+    """Re-escribir para que tome una cantidad arbitraria de parámetros.
+    Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists # noqa: E501 """
 
 # NO MODIFICAR - INICIO
 assert maximo_arbitrario(1, 10, 5, -5) == 10
